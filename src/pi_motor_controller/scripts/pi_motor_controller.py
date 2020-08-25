f78a23e0a5fd0874bc4d8ef5f28b652ff725e46f
Theo <theo@seelye.net>
2020-08-25 16:24:18 -0700

    addressed code review comments

--- src/pi_motor_controller/scripts/pi_motor_controller.py
+++ src/pi_motor_controller/scripts/pi_motor_controller.py
@@ -5,6 +5,7 @@ from geometry_msgs.msg import Twist
 from std_msgs.msg import Float32
 import RPi.GPIO as GPIO
 import enum
+import sys
 
 LINEAR_SPEED_SCALE_FACTOR = 100
 
@@ -31,7 +32,6 @@ class Motor(object):
         if power < 0 or power > 1:
             raise ValueError("set_power can only take a value between 0 and 1.")
         
-        # carryover from old script:
         # To achieve linear response use the square of the normalized speeds
         power *= abs(power)
         power *= LINEAR_SPEED_SCALE_FACTOR
@@ -43,7 +43,12 @@ class Motor(object):
         
 
 class Motor2Pin(Motor):
-    """class to control a motor (through an H-bridge) using 2 pins: pwm and direction."""
+    """class to control a motor (through an H-bridge) using 2 pins: pwm and direction.
+    This class acts as an abstraction layer for a GPIO interface for a motor controller
+    or H-bridge that uses sign-magnitude control via one pin providing PWM for magnitude
+    and the other being set to either high or low to specify the direction.
+    This class was specifically made for use with a Cytron MDD10A motor driver, but will
+    also work with other motor controllers and H-bridges that use the same system for control."""
     dir_pin = -1
     dir_forward_val = -1
     dir_backward_val = -1
@@ -54,6 +59,8 @@ class Motor2Pin(Motor):
         self.dir_forward_val = dir_forward_val
         self.dir_backward_val = GPIO.HIGH if dir_forward_val == GPIO.LOW else GPIO.LOW
         GPIO.setup(dir_pin, GPIO.OUT, initial=dir_forward_val)
+        print("Adding Motor2Pin - pwm_pin: " + pwm_pin + ", direction pin: " + dir_pin + 
+        "forward value: " + ("high" if dir_forward_val == GPIO.HIGH else "low"))
 
     def set(self, power):
         """sets power and direction, using power, a value between -1 (backwards) and 1 (forwards)"""
@@ -68,7 +75,13 @@ class Motor2Pin(Motor):
 
 
 class Motor3Pin(Motor):
-    """class to control a motor (through an H-bridge) using 3 pins: pwm, forward, and back."""
+    """class to control a motor (through an H-bridge) using 3 pins: pwm, forward, and back.
+    This class acts as an abstraction layer for a GPIO interface with a motor controller or
+    H-bridge that uses a variation of sign-magnitude control where one pin is used for 
+    magnitude control using PWM, and two pins are used to control the direction (a particular
+    pin is set high and the other low for one direction, and inverse for the opposite direction.)
+    This class was specifically made for use with L298N H-bridges, but will also work with 
+    other H-bridges or motor controllers that use the same control system."""
     forward_pin = -1
     backward_pin = -1
 
@@ -77,6 +90,11 @@ class Motor3Pin(Motor):
         self.forward_pin = forward_pin
         self.backward_pin = backward_pin
         GPIO.setup([forward_pin, backward_pin], GPIO.OUT, initial=GPIO.LOW)
+        print("Adding Motor3Pin - pwm_pin: " + pwm_pin + ", forward pin: " + forward_pin + ", backward "
+         + "pin:" + backward_pin)
+
+# print("adding motor" + "br" + ", pwm pin: " + str(22) + ", dir pin: " + str(17))
+
 
     def set(self, power):
         """sets power and direction, using power, a value between -1 (backwards) and 1 (forwards)"""
@@ -105,43 +123,52 @@ class ControlMode(enum.Enum):
 # ========================================================================================================
 # CONFIG
 
-# commented out because i don't know what these are for and they aren't used -theo
-# 
-# ANGULAR_SPEED_SCALE_FACTOR = 1/4    # Inverse of time minimum time to make one revolution
-# RADIANS_PER_REVOLUTION = 6.28319
-
-#same with these -theo
-#
-# w_dist = 0.23       # The separation between the drive wheels
-# lwheel_pub = None   # Publisher for left wheel speed
-# rwheel_pub = None   # Publisher for right wheel speed
-
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
 
 # Maximum allowable dutycycle
 MAX_ALLOWABLE_DC = 100
 
-# Hertz
-FREQ = 20000
+# PWM Frequency.
+# this is going to depend on your motors and H-bridge.
 
 # TODO: [for visibility]
 # !!! CONFIGURE THIS PER ROBOT !!!
 # actually todo: maybe use a config file of some kind to ensure this isn't run before being configured?
 #      it could be dangerous for someone to run this script unconfigured.
 motors = {}
+
+# EXAMPLE CONFIG: 
+#   This is the configuration for a mecanum-drive based robot using two Cytron MDD10A motor controllers.
+#   The Motor2Pin constructor takes 5 arguments: the pwm pin #, the direction pin #, the pin status 
+#   (GPIO.HIGH vs GPIO.LOW) that is "forward", the maximum allowable duty cycle (0-100, to avoid burning 
+#   out motors with too high of voltage), and the PWM freqency.
+#   See the Motor2Pin docstring for more info on this class.
 #
 # motors['fl'] = Motor2Pin(15, 24, GPIO.LOW, 100, FREQ)
-# rospy.loginfo("adding motor" + "fl" + ", pwm pin: " + str(15) + ", dir pin: " + str(25))
 # motors['bl'] = Motor2Pin(23, 14, GPIO.LOW, 100, FREQ)
-# rospy.loginfo("adding motor" + "bl" + ", pwm pin: " + str(23) + ", dir pin: " + str(14))
 # motors['fr'] = Motor2Pin(27, 18, GPIO.HIGH, 100, FREQ)
-# rospy.loginfo("adding motor" + "fr" + ", pwm pin: " + str(27) + ", dir pin: " + str(18))
 # motors['br'] = Motor2Pin(22, 17, GPIO.HIGH, 100, FREQ)
-# rospy.loginfo("adding motor" + "br" + ", pwm pin: " + str(22) + ", dir pin: " + str(17))
-
+#
 # the control mode to be used:
 # control_mode = ControlMode.mecanum
+
+# EXAMPLE CONFIG:
+#   This shows an example of how this script can be configured to control a robot that uses a tank-style
+#   chassis and an L298N H-bridge.
+#   the Motor3Pin constructor takes the same parameters as Motor2Pin, but instead of taking direction pin
+#   number and a value for which is forward, it takes a forward pin and backward pin number.
+#   for more info see the docstring for Motor3Pin.
+#
+# motors['l'] = Motor3Pin(15, 24, 23, 100, FREQ)
+# motors['r'] = Motor3Pin(14, 27, 18, 100, FREQ)
+#
+# the control mode to be used:
+# control_mode = ControlMode.tank
+
+
+# FIXME: uncomment out this next line once you've added your configuration.
+control_mode = None
 
 # ========================================================================================================
 
@@ -151,6 +178,16 @@ def StopMotors():
     # GPIO.output(dir_pins, GPIO.LOW)
     for motor in motors:
         motors[motor].stop()
+
+
+def set_and_log_pwr(motorname, power):
+    """double checks the given motor name is configured, clamps the power to the
+    range of -1 <= power <= 1, and sets the power using the motor object in motors."""
+    if not motorname in motors:
+        raise ValueError("No motor of name " + motorname + " configured!")
+    power = max(min(power, 1), -1)
+    rospy.loginfo('setting ' + motorname + 'power to: ' + str(power))
+    motors[motorname].set(power)
 
 
 def cmd_callback(cmdMessage):
@@ -164,43 +201,29 @@ def cmd_callback(cmdMessage):
         right_speed = 1.0 * linear_x - angular_z
         left_speed = 1.0 * linear_x + angular_z
 
-        right_speed = max(min(right_speed, 1), -1)
-        left_speed = max(min(left_speed, 1), -1)
-        
-        motors['l'].set(left_speed)
-        motors['r'].set(right_speed)
+        set_and_log_pwr("l", left_speed)
+        set_and_log_pwr("r", right_speed)
+    
     elif control_mode == ControlMode.mecanum:
         fr_speed = cmdMessage.linear.x - cmdMessage.linear.y + angular_z
         fl_speed = cmdMessage.linear.x + cmdMessage.linear.y - angular_z
         bl_speed = cmdMessage.linear.x - cmdMessage.linear.y - angular_z
         br_speed = cmdMessage.linear.x + cmdMessage.linear.y + angular_z
 
-        fr_speed = max(min(fr_speed, 1), -1)
-        fl_speed = max(min(fl_speed, 1), -1)
-        br_speed = max(min(br_speed, 1), -1)
-        bl_speed = max(min(bl_speed, 1), -1)
-        
-        rospy.loginfo("fr power: " + str(fr_speed))
-        rospy.loginfo("fl power: " + str(fl_speed))
-        rospy.loginfo("br power: " + str(br_speed))
-        rospy.loginfo("bl power: " + str(bl_speed))
+        set_and_log_pwr("fr", fr_speed)
+        set_and_log_pwr("fl", fl_speed)
+        set_and_log_pwr("bl", bl_speed)
+        set_and_log_pwr("br", br_speed)
 
-        motors['fr'].set(fr_speed)
-        motors['fl'].set(fl_speed)
-        motors['bl'].set(bl_speed)
-        motors['br'].set(br_speed)
-
-    # You should build in a deadman switch, however, the bThereClient already has one
-
-# removed for now since this needs re-implementing for scalable motor count.
-# def init_publisher():
-    # global lwheel_pub, rwheel_pub
-    # lwheel_pub = rospy.Publisher('lwheel_vtarget', Float32, queue_size=10)
-    # rwheel_pub = rospy.Publisher('rwheel_vtarget', Float32, queue_size=10)
+    # This script should optimally be using a deadman switch so 
+    # if whatever is publishing to the input topic breaks, the 
+    # robot doesn't go driving off ad infinitum
 
 
 def init_subscriber():
-    rospy.Subscriber('/cmd_vel/teleop', Twist, cmd_callback)
+    topic = rospy.get_param("~topic_name", "/cmd_vel/teleop")
+    rospy.loginfo("listening for input on " + topic)
+    rospy.Subscriber(topic, Twist, cmd_callback)
 
 
 def create_node():
@@ -208,13 +231,13 @@ def create_node():
 
 
 if __name__ == '__main__':
-    # try:
-        # init_publisher()
-    # except rospy.ROSInterruptException:
-        # pass
+    rospy.logerr("cannot run rpi motor controller, control_mode not set!" 
+                + "\nMake sure you have configured the script before use!")
+    sys.exit()
 
     create_node()
     init_subscriber()
+    rospy.loginfo("initialized subscriber")
     rospy.spin()
     StopMotors()
     GPIO.cleanup()
