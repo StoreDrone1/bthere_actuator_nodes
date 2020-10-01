28765b5729af2b585806824927cdca9b4d1cfb11
Stuart Marshall <stuart@bthere.ai>
2020-09-30 20:32:46 -0700

    Modifying constants to get local script directory for config file path

--- src/maestro_servo_controller/scripts/constant.py
+++ src/maestro_servo_controller/scripts/constant.py
@@ -2,9 +2,10 @@ __author__ = "Matt Wenger and Stuart Marshall"
 __copyright__ = "Copyright 2019, bThere.ai"
 
 # Version and name strings
+import os
 VERSION = 'v0.0.1'
 NAME = 'bthere_servos'
 
 # General system constants
-ETC_DIR = './'
-DEFAULT_CONFIG_FILE = ETC_DIR + 'bthere.cfg'
+dir_path = os.path.dirname(os.path.realpath(__file__))
+DEFAULT_CONFIG_FILE = dir_path + '/bthere.cfg'
