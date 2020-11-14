cd7c3fee530e84d1ba64af32d190f9f16fae2ebf
StoreDrone1 <61709896+StoreDrone1@users.noreply.github.com>
2020-11-14 12:39:24 -0500

    Update servos.py
    Added a second callback and topic for servo2.

--- src/maestro_servo_controller/scripts/servos.py
+++ src/maestro_servo_controller/scripts/servos.py
@@ -13,25 +13,42 @@ import maestro_servos
 increment_scalar = 1
 
 
-def callback(twist):
+def callback1(twist):
     rospy.loginfo(rospy.get_caller_id() + " Received: " + str(twist))
     pan = int(twist.angular.z * increment_scalar)
     tilt = int(twist.angular.y * increment_scalar)
     if (pan == 0 and tilt == 0):
         return
     bthere_log.i('Pan incrementing by ' + str(pan) +
-                 ' Tilt incrementing by ' + str(tilt))
-    maestro_servos.panTilt(pan, tilt)
+                 ' Tilt incrementing by ' + str(tilt) + 
+                 ' on servo 1 ')
+    maestro_servos.panTilt(pan, tilt, 1)
+    
+def callback2(twist):
+    rospy.loginfo(rospy.get_caller_id() + " Received: " + str(twist))
+    pan = int(twist.angular.z * increment_scalar)
+    tilt = int(twist.angular.y * increment_scalar)
+    if (pan == 0 and tilt == 0):
+        return
+    bthere_log.i('Pan incrementing by ' + str(pan) +
+                 ' Tilt incrementing by ' + str(tilt) +
+                 ' on servo 2 ')
+    maestro_servos.panTilt(pan, tilt, 2)    
 
 
-def listener(topic):
+def listener(topic1, topic2):
     rospy.init_node('bthere_maestro_servos')
-    rospy.Subscriber(topic, Twist, callback)
+    rospy.Subscriber(topic1, Twist, callback1)
+    rospy.Subscriber(topic2, Twist, callback2)
     rospy.spin()
 
 
 def main():
+    global top1
+    global top2
+    
     bthere_log.i("bthere_servos running...")
-    topic = rospy.get_param("~topic_name", "/camera_servo1/teleop")
+    top1 = rospy.get_param("~topic_name", "/camera_servo1/teleop")
+    top2 = rospy.get_param("~topic_name", "/camera_servo2/teleop")
     maestro_servos.setup()
-    listener(topic)
+    listener(top1, top2)
